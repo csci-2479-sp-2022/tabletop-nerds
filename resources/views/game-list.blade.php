33f2d59b7f0d80ffc540abad3241829d643ec039
benwagner813 <benwagner813@gmail.com>
2022-02-18 09:40:27 -0500

    Created a game model, game-list view, game controller, updated routes and the nav bar

---
+++ resources/views/game-list.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Games') }}
+        </h2>    
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                @foreach($games as $game)
+                <div class="flex p-6 bg-white border-b border-gray-200 justify-between items-center">
+                    <div class="text-xl">{{$game['name']}}</div>
+                    <div class="px-6">{{$game['description']}}</div>
+                    <div class="">{{$game['rating']}}</div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</x-app-layout>
