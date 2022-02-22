23bdce35d4144bb68eb8ce0d2aed5b135edd9697
Aiden Smith <29802327+Voxel20@users.noreply.github.com>
2022-02-22 10:46:26 -0500

    Fixed onload page issue. (#14)
    Rerouted the initial page to 'welcome' instead of 'login' to allow for easier access to the 'register' page.

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return view('welcome');
+});
 
 
 Route::get('/dashboard', function () {
