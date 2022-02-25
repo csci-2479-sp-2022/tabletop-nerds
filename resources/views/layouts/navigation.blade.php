00031aacbae27dd52c393352a03f9aa5c14dd0d6
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-24 22:05:03 -0500

    Made some changes in the navigation.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
 
-                    
+
                     <x-nav-link :href="route('games')" :active="request()->routeIs('games')">
                         {{ __('Game List') }}
                     </x-nav-link>
@@ -41,43 +41,42 @@
             <div class="hidden sm:flex sm:items-center sm:ml-4">
 
                 @auth
-                    <x-dropdown align="right" width="48" class="">
-                        <x-slot name="trigger">
-                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                <div>{{ Auth::user()->name }}</div>
+                <x-dropdown align="right" width="48" class="">
+                    <x-slot name="trigger">
+                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            <div>{{ Auth::user()->name }}</div>
 
-                                <div class="ml-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                            </button>
-                        </x-slot>
+                            <div class="ml-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
 
-                        <x-slot name="content">
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
+                    <x-slot name="content">
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <x-dropdown-link :href="route('profile')">
+                                {{ __('Profile') }}
+                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('wishlist')">
+                                {{ __('My Wishlist') }}
+                            </x-dropdown-link>
 
-                                <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                            <x-dropdown-link :href=" route('logout')" onclick="event.preventDefault();
                                                     this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                                
-                            </form>
-                            <x-dropdown-link :href="route('profile')" onclick="event.preventDefault();
-                                                    this.closest('form').submit();">
-                                    {{ __('Profile') }}
-                                </x-dropdown-link>
-                            <x-responsive-nav-link :href="route('wishlist')">
-                                     {{ __('My Wishlist') }}
-                               </x-responsive-nav-link>
-                        </x-slot>
-                    </x-dropdown>
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+
+                    </x-slot>
+                </x-dropdown>
                 @else
-                    <a href="{{ route('login') }}" class="text-sm underline">Log in</a>
-                    &nbsp;|&nbsp;
-                    <a href="{{ route('register') }}" class="text-sm underline">Register</a>
+                <a href="{{ route('login') }}" class="text-sm underline">Log in</a>
+                &nbsp;|&nbsp;
+                <a href="{{ route('register') }}" class="text-sm underline">Register</a>
                 @endauth
             </div>
 
@@ -101,54 +100,49 @@
 
                 <input type="search" class="form-control relative flex-auto block w-auto px-3 py-1.5 text-base font-normal text-black-700 bg-white bg-clip-padding border border-solid border-gray-500 rounded transition ease-in-out m-0 focus:text-black-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
 
-
                 <button type="submit" class="btn px-4 py-4 bg-black-600 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-300 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
                     </svg>
                 </button>
             </form>
-            <x-responsive-nav-link :href="route('profile')" :active="request()->routeIs('profile')">
-                {{ __('Profile') }}
-            </x-responsive-nav-link>
 
-
-            <x-responsive-nav-link :href="route('games')" :active="request()->routeIs('games')">
-                {{ __('Game List') }}
-            </x-responsive-nav-link>
-
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            @auth
+            <!-- Responsive Settings Options -->
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                @auth
                 <div class="px-4">
                     <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                     <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
                 </div>
-
-
-
-            <div class="mt-3 space-y-1">
-                <!-- Authentication -->
-                <x-responsive-nav-link :href="route('wishlist')">
-                    {{ __('My Wishlist') }}
-                </x-responsive-nav-link>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-
+                <div class="mt-3 space-y-1">
+                    <!-- Authentication -->
+                    <x-responsive-nav-link :href="route('games')" :active="request()->routeIs('games')">
+                        {{ __('Game List') }}
                     </x-responsive-nav-link>
-                </form>
-                @else
-                <a href="{{ route('login') }}" class="text-sm underline">Log in</a>
-                &nbsp;|&nbsp;
-                <a href="{{ route('register') }}" class="text-sm underline">Register</a>
-            @endauth
+                    <x-responsive-nav-link :href="route('wishlist')">
+                        {{ __('My Wishlist') }}
+                    </x-responsive-nav-link>
+                    <x-responsive-nav-link :href="route('profile')" :active="request()->routeIs('profile')">
+                        {{ __('Profile') }}
+                    </x-responsive-nav-link>
 
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                            {{ __('Log Out') }}
+
+                        </x-responsive-nav-link>
+                    </form>
+
+                    @else
+                    <a href="{{ route('login') }}" class="text-sm underline">Log in</a>
+                    &nbsp;|&nbsp;
+                    <a href="{{ route('register') }}" class="text-sm underline">Register</a>
+                    @endauth
+                </div>
             </div>
+            
         </div>
     </div>
 </nav>
